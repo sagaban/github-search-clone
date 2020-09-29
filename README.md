e5460a1616e7e3a56afb1b3fbb6e7a64dc05cf22
Santiago Bandiera <sagaban@hotmail.com>
2020-09-28 21:18:13 -0300

    Update name and add config to deploy

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# github-search
+# GitHub Search Clone
 
 ## Project setup
 ```
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
+const isProduction = process.env.NODE_ENV === "production";
+
 module.exports = {
+  publicPath: isProduction ? "/github-search-clone/" : "/",
   pluginOptions: {
     quasar: {
       importStrategy: "kebab",
